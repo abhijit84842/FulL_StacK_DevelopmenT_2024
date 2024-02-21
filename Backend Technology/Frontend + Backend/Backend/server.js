1bae9d2bebbb77885f28411b1908101207824876
Abhijit Das <abhijitd721201@gmail.com>
2024-02-21 12:49:08 +0530

    update

--- Backend Technology/Frontend + Backend/Backend/server.js
+++ Backend Technology/Frontend + Backend/Backend/server.js
@@ -1,15 +1,46 @@
-require('dotenv').config()
-const express= require('express')  
+require("dotenv").config();
+const express = require("express");
 
-
-const app = express()
+const app = express();
 
 //const port =4000
 
-app.get('/', (req,res)=>{
-    res.send("Server is Ready..")
-})
+app.get("/", (req, res) => {
+  res.send("Server is Ready..");
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log("Port set " + process.env.PORT)
-})
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A jokes",
+      content: " This is a jokes",
+    },
+    {
+        id: 2,
+        title: "A jokes",
+        content: " This is a jokes",
+      },
+      {
+        id: 3,
+        title: "A jokes",
+        content: " This is a jokes",
+      },
+      {
+        id: 4,
+        title: "A jokes",
+        content: " This is a jokes",
+      },
+      {
+        id: 5,
+        title: "A jokes",
+        content: " This is a jokes",
+      },
+  ];
+
+  res.send(jokes)
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Port set " + process.env.PORT);
+});

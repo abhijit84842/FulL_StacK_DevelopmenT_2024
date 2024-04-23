4d1217e424b4b5624cd89b631074bd71b35965e9
Abhijit Das <abhijitd721201@gmail.com>
2024-04-23 11:48:49 +0530

    create component in component

--- React + Framework/Next Js 2024/first_app_in_nextjs/app/page.js
+++ React + Framework/Next Js 2024/first_app_in_nextjs/app/page.js
@@ -1,4 +1,12 @@
 const Page = () => {
-  return <div className="main-container">Hi This is Next Js by Abhijit</div>;
+  return <div className="main-container">
+    <NewComp name="Abhijit Das"/>
+    <div>This is Next Js by Abhijit 2024</div>
+  </div>;
 };
 export default Page;
+
+// create a new component in this page..
+const NewComp=({name})=>{
+  return <h1>Hi i am {name} </h1>
+}

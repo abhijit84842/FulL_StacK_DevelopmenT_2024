b2aa1aaca5ece854028192b17fa9902b460aae69
Abhijit Das <abhijitd721201@gmail.com>
2024-04-24 17:42:38 +0530

    update

--- React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/about/page.js
+++ React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/about/page.js
@@ -1,11 +1,14 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
 
- const About = () => {
+const About = () => {
+  const router = useRouter();
   return (
     <div>
       <h1>Information about this page</h1>
+      <button onClick={() => router.push("/")}>Go to Home Page</button>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/page.js
+++ React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/page.js
@@ -6,6 +6,10 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
+
+  const Navigation = (name) => {
+    router.push(name);
+  };
   return (
     <main>
       <div>
@@ -16,7 +20,8 @@ export default function Home() {
         <div>
           <Link href="/about">Go to about Page</Link>
         </div>
-        <button onClick={() => router.push("/login")}>Login Button</button>
+        <button onClick={() => Navigation("/login")}>Login Button</button>
+        <button onClick={() => Navigation("/about")}>About Button</button>
       </div>
     </main>
   );

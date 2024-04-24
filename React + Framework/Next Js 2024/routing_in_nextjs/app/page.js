ef1626ee0a684ada8b694dc459cd5f992fb7a9e2
Abhijit Das <abhijitd721201@gmail.com>
2024-04-24 16:57:15 +0530

    basic routing

--- React + Framework/Next Js 2024/routing_in_nextjs/app/page.js
+++ React + Framework/Next Js 2024/routing_in_nextjs/app/page.js
@@ -1,8 +1,12 @@
-
+import About from "./about/page";
 
 export default function Home() {
   return (
     <main>
+      <div>
+        <h1>This is Home Page</h1>
+        <About/>
+      </div>
     </main>
   );
 }

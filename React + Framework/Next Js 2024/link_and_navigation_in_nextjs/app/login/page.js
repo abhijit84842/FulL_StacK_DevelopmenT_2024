396fa65fcd89e3fada8667a91e3dff0d7d89c9c9
Abhijit Das <abhijitd721201@gmail.com>
2024-04-24 17:17:24 +0530

    link set

---
+++ React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/login/page.js
@@ -0,0 +1,7 @@
+const LoginPage=()=>{
+    return <div>
+        <h1>This is login Page</h1>
+    </div>
+}
+
+export default LoginPage
--- React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/page.js
+++ React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/page.js
@@ -1,9 +1,16 @@
-
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <main >
-      
+    <main>
+      <div>
+        <h1>This is Home page</h1>
+        <div>
+          <Link href="/login">Go to Login Page</Link>
+        </div>
+        <div>
+          <Link href="/about">Go to about Page</Link>
+        </div>
+      </div>
     </main>
   );
 }

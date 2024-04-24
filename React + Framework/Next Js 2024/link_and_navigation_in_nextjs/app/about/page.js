e6ee41bca99e09e9cb716733c29a5b24cf8a0c01
Abhijit Das <abhijitd721201@gmail.com>
2024-04-24 18:11:55 +0530

    update

--- React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/about/page.js
+++ React + Framework/Next Js 2024/link_and_navigation_in_nextjs/app/about/page.js
@@ -7,6 +7,12 @@ const About = () => {
     <div>
       <h1>Information about this page</h1>
       <button onClick={() => router.push("/")}>Go to Home Page</button>
+      <button onClick={() => router.push("/about/aboutstudent")}>
+        About Students
+      </button>
+      <button onClick={() => router.push("/about/aboutcollege")}>
+        About college
+      </button>
     </div>
   );
 };

e2b9ad9d3cdd9b90d80a06e5d09d4d96f9da212e
Abhijit Das <abhijitd721201@gmail.com>
2024-04-25 17:38:58 +0530

    add new component and use middleware

--- React + Framework/Next Js 2024/middleware_in_next_js/middleware.js
+++ React + Framework/Next Js 2024/middleware_in_next_js/middleware.js
@@ -3,6 +3,11 @@ import { NextResponse } from "next/server"
 export function middleware(request){
     // console.log(request)
 
-    if(request.nextUrl.pathname != "/login")
+    // if(request.nextUrl.pathname != "/login")
     return NextResponse.redirect(new URL("/login",request.url))
 }
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/about/:path*',
+  }

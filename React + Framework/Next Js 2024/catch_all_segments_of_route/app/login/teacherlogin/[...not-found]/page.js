d36267d47400008ce2da7e7efc161dc3eeb48ed3
Abhijit Das <abhijitd721201@gmail.com>
2024-04-25 15:33:19 +0530

    route specific 404 page

---
+++ React + Framework/Next Js 2024/catch_all_segments_of_route/app/login/teacherlogin/[...not-found]/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+const TeacherNotFound = () => {
+  return (
+    <div>
+        <h1>Teacher this Page not Available.......</h1>
+    </div>
+  )
+}
+
+export default TeacherNotFound
+
+// here i use Segments to make Route Specific 404 page.

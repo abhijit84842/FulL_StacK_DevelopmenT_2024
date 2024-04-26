d048fa355d454c57596ddcb7e76855719e45d754
Abhijit Das <abhijitd721201@gmail.com>
2024-04-26 19:21:19 +0530

    change title of individual page

---
+++ React + Framework/Next Js 2024/generatemetadata_for_dynamicmetadata/app/userinfo/page.js
@@ -0,0 +1,18 @@
+
+const UserInfo =()=>{
+    return <div>
+        <h1>User information</h1>
+    </div>
+}
+export default UserInfo
+
+
+// we have to keep this meta comp as it is
+ export function generateMetadata(){
+    return {
+        title:"User-info",
+        //we can also add description
+        description: "User page description by Abhi"
+    }
+
+ }

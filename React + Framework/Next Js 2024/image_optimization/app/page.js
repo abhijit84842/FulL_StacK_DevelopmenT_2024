3cdb12353d1e0fdb92be7b27897af6fc72982e71
Abhijit Das <abhijitd721201@gmail.com>
2024-04-26 18:23:32 +0530

    load image optimization

--- React + Framework/Next Js 2024/image_optimization/app/page.js
+++ React + Framework/Next Js 2024/image_optimization/app/page.js
@@ -1,14 +1,20 @@
 import Image from "next/image";
-import profile from "../public/vercel.svg"
+import profile from "../public/vercel.svg";
 
 export default function Home() {
   return (
     <main>
       <h1>Image Optimization in Next Js</h1>
-      <Image 
-      src={profile}
-      height={300}
-      weight={300}
+      {/* <Image src={profile} height={100} weight={100} /> */}
+
+      {/* <img src={profile.src} alt="loading" /> */}
+
+      <Image
+        src={
+          "https://images.pexels.com/photos/1183099/pexels-photo-1183099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        width={500}
+        height={500}
       />
     </main>
   );

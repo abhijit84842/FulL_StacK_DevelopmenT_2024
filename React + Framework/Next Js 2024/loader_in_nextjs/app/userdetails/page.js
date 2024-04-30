ebbee64ec45a196f6e1f5607502ae94da0fec91e
Abhijit Das <abhijitd721201@gmail.com>
2024-04-30 14:48:47 +0530

    add loading spinner

--- React + Framework/Next Js 2024/loader_in_nextjs/app/userdetails/page.js
+++ React + Framework/Next Js 2024/loader_in_nextjs/app/userdetails/page.js
@@ -1,35 +1,41 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import UserInfo from "./userinfo/page";
+import Loader from "./loading/page";
 
 
 
 const UserDetails = () => {
+  const [userData, setUserData] = useState([]);
+  const [loadingBar, setLoadinBar] = useState(false);
 
-  const [userData,setUserData]=useState([])
-
+  
   const callApi = async () => {
+    setLoadinBar(true)
     let res = await fetch("https://dummyjson.com/users");
     let data = await res.json();
-    console.log(data.users)
-    setUserData(data.users)
-    
+    // console.log(data.users)
+    setUserData(data.users);
+    setLoadinBar(false);
   };
-  
-  useEffect(()=>{
-    callApi()
-  },[])
+
+  // useEffect(() => {
+
+  // ;
+  // }, []);
 
   return (
     <div>
       <h1>All user details are here..</h1>
       <Link href="/">Back to Home Page</Link>
-      {
-        userData.map((list)=>(
-          <UserInfo key={list.id} list={list}/>
-        ))
-      }
+      <button onClick={()=>callApi()}>Load Data</button>
+    
+      {loadingBar ? (
+        <Loader />
+      ) : (
+        userData.map((list) => <UserInfo key={list.id} list={list} />)
+      )}
     </div>
   );
 };

96753307b1b750b18d0321d05369f7f73d2b702b
Abhijit Das <abhijitd721201@gmail.com>
2024-05-01 12:58:47 +0530

    apply condition on api

--- React + Framework/Next Js 2024/36_get_api_with_static_data_in_api_route/app/api/users/[id]/route.js
+++ React + Framework/Next Js 2024/36_get_api_with_static_data_in_api_route/app/api/users/[id]/route.js
@@ -6,7 +6,8 @@ export function GET(request,response){
     const data=user
 
     const userData= data.filter((item)=>item.id==response.params.id)
-    return NextResponse.json(userData,{status:200})
+    return NextResponse.json(userData==0 ? {result: "No Data Found",success:false} : {result: userData , success:true}
+    ,{status:200}) // apply condition if userData is empty Array.
 }
 
 // show indivisual data

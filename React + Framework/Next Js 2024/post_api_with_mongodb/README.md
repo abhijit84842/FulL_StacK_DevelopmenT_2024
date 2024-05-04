a8fab8e5e8427c546c040042da76b6ca90d9b07a
Abhijit Das <abhijitd721201@gmail.com>
2024-05-04 18:34:04 +0530

    post req with mongoDB

--- React + Framework/Next Js 2024/post_api_with_mongodb/README.md
+++ React + Framework/Next Js 2024/post_api_with_mongodb/README.md
@@ -1,3 +1,11 @@
 # Connect with DB using mongoose.
 
 // GET APi call to fetch data from database.
+
+# Save Data in DB with POST API
+
+- Make Function for POST Api
+- Write code for saving data
+- Get data from API Request
+- Save product data in MongoDB
+- Test Api with Postman.
--- React + Framework/Next Js 2024/post_api_with_mongodb/app/api/products/route.js
+++ React + Framework/Next Js 2024/post_api_with_mongodb/app/api/products/route.js
@@ -2,11 +2,13 @@ import { Product } from "@/app/lib/model/product";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
+const url =
+"mongodb+srv://mongodb:hhSkR0HjNOnHhJhJ@cluster0.cupwun4.mongodb.net/abhijitdb?retryWrites=true&w=majority&appName=Cluster0";
+// GET Api method
 export async function GET(request, response) {
 
     // Connect with DataBase using Mongoose
-  const url =
-    "mongodb+srv://mongodb:hhSkR0HjNOnHhJhJ@cluster0.cupwun4.mongodb.net/abhijitdb?retryWrites=true&w=majority&appName=Cluster0";
+
     let data =[]
   try {
     await mongoose.connect(url);
@@ -18,4 +20,27 @@ export async function GET(request, response) {
   }
 
   return NextResponse.json({ result: data ,success:true});
+}
+
+
+
+
+// POST API Method with mongodb
+export async function POST(request,response){
+    
+   
+    try{
+        let payload= await request.json()       // received post request from user
+        await mongoose.connect(url)
+        console.log("POST - Connected Successfully")
+
+        let product=new Product( payload)
+
+        let result = await product.save()
+        return NextResponse.json({result, success:true})
+        
+    }catch(error){
+        console.log("POST- not connected")
+    }
+
 }

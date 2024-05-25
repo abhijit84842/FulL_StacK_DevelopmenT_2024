8596b7ee01556b2a8adfcc7fdc1c55d570f6ee57
Abhijit Das <abhijitd721201@gmail.com>
2024-05-25 11:27:18 +0530

    create server and api route using express

---
+++ Node Js/Expressjs_Routing_Middleware/README.md
@@ -0,0 +1,12 @@
+\* Express.js and FrameWork
+
+- intro of express.js
+  => its a framework(flow)
+  => Manages everything from receiving the request and giving back the response.
+- setting up a basic express application
+
+\* Routing
+
+\* Request and response handling
+
+\* Error Handling
---
+++ Node Js/Expressjs_Routing_Middleware/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const app = express();
+
+// create API Routes...
+app.get("/", (req, res) => {
+  res.send("Server is ready...");
+});
+
+
+// create API Routes...
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A jokes",
+      constent: "This is jokes",
+    },
+    {
+      id: 2,
+      title: "A jokes",
+      content: " This is a jokes",
+    },
+    {
+      id: 3,
+      title: "A jokes",
+      content: " This is a jokes",
+    },
+    {
+      id: 4,
+      title: "A jokes",
+      content: " This is a jokes",
+    },
+    {
+      id: 5,
+      title: "A jokes",
+      content: " This is a jokes",
+    },
+  ];
+
+  res.send(jokes)
+});
+
+app.listen(3000 , ()=>{
+    console.log("Port set " +3000)
+})

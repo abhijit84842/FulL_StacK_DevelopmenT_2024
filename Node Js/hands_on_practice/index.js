e3dd4470591b316e6ad85419cd90ec49f63fc3c5
Abhijit Das <abhijitd721201@gmail.com>
2024-05-29 16:05:55 +0530

    write logic

--- Node Js/hands_on_practice/index.js
+++ Node Js/hands_on_practice/index.js
@@ -1,27 +1,32 @@
-const express = require('express')
-const path= require('path')
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-const app = express()
+const app = express();
 
 // setup view engine....
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 
 // readable format
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //static file
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/" , (req , res)=>{
-    res.send("server is running...")
-})
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
 
-app.get("/about" ,(req,res)=>{
-    res.render("about")
-})
+app.get("/about", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    //    console.log(files)
 
+    // we can also send data as object in about page
+    res.render("about", { files: files });
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("Port number is => "+3000)
-})
+app.listen(3000, () => {
+  console.log("Port number is => " + 3000);
+});

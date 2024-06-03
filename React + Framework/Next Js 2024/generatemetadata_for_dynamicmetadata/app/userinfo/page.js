9f182a9eff8947371cb5ab43b848e421e0077e9c
Abhijit Das <abhijitd721201@gmail.com>
2024-06-03 15:52:55 +0530

    add some description

--- React + Framework/Next Js 2024/generatemetadata_for_dynamicmetadata/app/userinfo/page.js
+++ React + Framework/Next Js 2024/generatemetadata_for_dynamicmetadata/app/userinfo/page.js
@@ -8,6 +8,7 @@ export default UserInfo
 
 
 // we have to keep this meta comp as it is
+// we can't use dynamic meta data method with the client component, this method only allow in server component.
  export function generateMetadata(){
     return {
         title:"User-info",

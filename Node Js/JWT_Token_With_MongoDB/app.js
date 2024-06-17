391af73dff8e98244f11298e60dd0f4a183e4588
Abhijit Das <abhijitd721201@gmail.com>
2024-06-17 19:40:37 +0530

    authentication done

--- Node Js/JWT_Token_With_MongoDB/app.js
+++ Node Js/JWT_Token_With_MongoDB/app.js
@@ -1,112 +1,105 @@
-const express = require("express")
-const path =require('path')
-const cookieParser= require('cookie-parser')
-const { default: mongoose } = require("mongoose")
-const bcrypt= require('bcrypt')
-const jwt=require("jsonwebtoken")
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const UserModel= require("./models/userModel")
+const UserModel = require("./models/userModel");
 
-const app =express()
-
+const app = express();
 
 // to read the json data comming from frontend
-app.use(express.json())
+app.use(express.json());
 
 // to read the form data comming from frontend
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 // setup public file To use CSS , javascript etc file
-app.use(express.static(path.join(__dirname , "public" )))
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // set up view engine for server side rendering.....
-app.set("view engine" , "ejs")
+app.set("view engine", "ejs");
 
 // to see the cookie str inbackend
-app.use(cookieParser())
+app.use(cookieParser());
 
 // connection str of MongoDB
-const url="mongodb+srv://abhijitbackend:abhijit84842@cluster0.l4vqt9f.mongodb.net/JwtAuthDB?retryWrites=true&w=majority&appName=Cluster0"
+const url =
+  "mongodb+srv://abhijitbackend:abhijit84842@cluster0.l4vqt9f.mongodb.net/JwtAuthDB?retryWrites=true&w=majority&appName=Cluster0";
 
 //
-app.get("/" , (req,res)=>{
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.post("/accreate" , (req,res)=>{  
-    // console.log(req.body)
-    const {name ,age ,email, password}= req.body
-    
+app.post("/accreate", (req, res) => {
+  // console.log(req.body)
+  const { name, age, email, password } = req.body;
 
-    // make salt to encrypte password
-    bcrypt.genSalt(10, (err,salt)=>{
-        bcrypt.hash(password, salt ,async(err, hash)=>{         // makeing hash
-            // console.log(hash)
+  // make salt to encrypte password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      // makeing hash
+      // console.log(hash)
 
-            try{
-                await mongoose.connect(url)
-                console.log("DB connected Successfully....")
-            }catch(err){
-                console.log("DB not connected....")
-            }
-        
-            let result=await UserModel.create({name , age , email , password: hash })
+      try {
+        await mongoose.connect(url);
+        console.log("DB connected Successfully....");
+      } catch (err) {
+        console.log("DB not connected....");
+      }
 
-            // set cookie in client web browser using jwt token
-            const token=jwt.sign({email:email}, "secret")
-            res.cookie("token" ,token)
-            
-            res.redirect("/")
-        })
-    })
-    
-    
-})
+      let result = await UserModel.create({ name, age, email, password: hash });
+
+      // set cookie in client web browser using jwt token
+      const token = jwt.sign({ email: email }, "secret");
+      res.cookie("token", token);
+
+      res.redirect("/");
+    });
+  });
+});
 
 //
-app.get("/login", (req,res)=>{
-    res.render("login")
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-// login and check Authantication 
-app.post("/login" , async (req,res)=>{
-    // console.log(req.body)
-    // let {email , password}= req.body
+// login and check Authantication
+app.post("/login", async (req, res) => {
+  // console.log(req.body)
+  // let {email , password}= req.body
 
-    try{
-        await mongoose.connect(url)
+  try {
+    await mongoose.connect(url);
+  } catch (err) {
+    console.log("DB not connected");
+  }
+  // check email exists or not in db
+  let user = await UserModel.findOne({ email: req.body.email });
+  if (!user) {
+    return res.send("wrong");
+  }
 
-    }catch(err){
-        console.log("DB not connected")
+  bcrypt.compare(req.body.password, user.password, function (err, result) {
+    // result == true
+    if (result) {
+      let token = jwt.sign({ email: user.email }, "secrectkey");        // create jwt token to create encrypted cookie
+      res.cookie("token", token);
+      res.send("successfully login");
+    } else {
+      return res.send("incorrect password");
     }
-    // check email exists or not in db
-    let user=await UserModel.findOne({email:req.body.email})
-    if(!user){
-        return res.send("some thing went wrong")
-    }else{
-        // compare the user enterd password
-        bcrypt.compare(req.body.password, user.password, function(err, result) {
-            // result == true
-            if(result){
-                console.log("Successfully login")
-            }else{
-                console.log("password is incorrect")
-            }
-        });
-    }
-
-    res.render("login")
-})
+  });
+});
 
 // logout by removing the token
-app.get("/logout" , (req,res)=>{
-    res.cookie("token" , "")        // blank the token value
-    res.redirect("/")
-})
+app.get("/logout", (req, res) => {
+  res.cookie("token", ""); // blank the token value
+  res.redirect("/");
+});
 
-
-
-app.listen(3000,()=>{
-    console.log("Port number is => "+3000)
-})
+app.listen(3000, () => {
+  console.log("Port number is => " + 3000);
+});

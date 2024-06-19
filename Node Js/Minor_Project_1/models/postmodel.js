e81c904013e85aba4605b8078ee4965ab49af10a
Abhijit Das <abhijitd721201@gmail.com>
2024-06-19 18:50:26 +0530

    update postmodel

--- Node Js/Minor_Project_1/models/postmodel.js
+++ Node Js/Minor_Project_1/models/postmodel.js
@@ -1,13 +1,17 @@
 const { default: mongoose } = require("mongoose");
 
 const postSchema = mongoose.Schema({
-    postdata:String,
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"user"
-    },
-    date: {
-        type:Date,
-        default:Date.now()
-    }
-})
+  content: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+});
+
+
+module.exports = mongoose.model("post" , postSchema)

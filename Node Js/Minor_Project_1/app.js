bd4f6966c44e12cb24e678f786f0a38e34e4fd8c
Abhijit Das <abhijitd721201@gmail.com>
2024-06-19 17:10:49 +0530

    create post api

--- Node Js/Minor_Project_1/app.js
+++ Node Js/Minor_Project_1/app.js
@@ -3,6 +3,7 @@ const path =require('path')
 
 // import models
 const userModel= require('./models/usermodel')
+const { default: mongoose } = require('mongoose')
 
 
 const app = express()
@@ -17,9 +18,24 @@ app.use(express.static(path.join(__dirname,"public")))
 // set view engine
 app.set("view engine" ,"ejs")
 
+// connection Str
+const url =
+  "mongodb+srv://abhijitbackend:abhijit84842@cluster0.l4vqt9f.mongodb.net/MinorProjectDB?retryWrites=true&w=majority&appName=Cluster0";
+
 
 app.get("/",(req,res)=>{
     res.render("index")
+})
+
+app.post("/create" ,async (req,res)=>{
+    //mongodb connect
+    try{
+        await mongoose.connect()
+    }catch(err){
+        console.log("DB not connected")
+    }
+    const userData=await userModel.create({})
+    res.send(userData)
 })
 
 app.listen(3000,()=>{

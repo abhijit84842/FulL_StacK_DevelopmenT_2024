b6db6b780a832110e7db1894ab7739b5957b4281
Abhijit Das <abhijitd721201@gmail.com>
2024-06-19 18:19:18 +0530

    save user data in db

--- Node Js/Minor_Project_1/app.js
+++ Node Js/Minor_Project_1/app.js
@@ -1,50 +1,55 @@
-const express = require('express')
-const path =require('path')
-
+const express = require("express");
+const path = require("path");
 // import models
-const userModel= require('./models/usermodel')
-const { default: mongoose } = require('mongoose')
+const userModel = require("./models/usermodel");
+const { default: mongoose } = require("mongoose");
 
-
-const app = express()
+const app = express();
 
 // json
-app.use(express.json())
+app.use(express.json());
 //url data
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 // for static file
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 // set view engine
-app.set("view engine" ,"ejs")
+app.set("view engine", "ejs");
 
 // connection Str
 const url =
   "mongodb+srv://abhijitbackend:abhijit84842@cluster0.l4vqt9f.mongodb.net/MinorProjectDB?retryWrites=true&w=majority&appName=Cluster0";
 
+// renderin the home page
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
-
-app.get("/user/create", (req,res)=>{
-    res.render("registerd")
-})
-
+// rendering the registered page
+app.get("/user/create", (req, res) => {
+  res.render("registerd");
+});
 
 // POST API for User ac Create
-app.post("/create" ,async (req,res)=>{
-    //mongodb connect
-    try{
-        await mongoose.connect()
-    }catch(err){
-        console.log("DB not connected")
-    }
-    const userData=await userModel.create({})
-    res.send(userData)
-})
+app.post("/create", async (req, res) => {
+  // console.log(req.body)
+  let { username, name, email, password, age } = req.body;
+  // mongodb connect
+  try {
+    await mongoose.connect(url);
+  } catch (err) {
+    console.log("DB not connected");
+  }
+  const userData = await userModel.create({
+    username,
+    name,
+    email,
+    password,
+    age,
+  });
+  res.render("index");
+});
 
-
-app.listen(3000,()=>{
-    console.log("PORT=> "+3000)
-})
+app.listen(3000, () => {
+  console.log("PORT=> " + 3000);
+});

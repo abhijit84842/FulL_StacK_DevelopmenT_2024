6cf4130c47f810fc0847ca793a95d5b0ccc0a962
Abhijit Das <abhijitd721201@gmail.com>
2024-06-19 16:43:01 +0530

    app setup complete

---
+++ Node Js/Minor_Project_1/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path =require('path')
+
+const app = express()
+
+// json
+app.use(express.json())
+//url data
+app.use(express.urlencoded({extended:true}))
+// for static file
+app.use(express.static(path.join(__dirname,"public")))
+
+// set view engine
+app.set("view engine" ,"ejs")
+
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.listen(3000,()=>{
+    console.log("PORT=> "+3000)
+})

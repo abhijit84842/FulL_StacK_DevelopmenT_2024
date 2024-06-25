28e9744ff01eb539ea02ee8382cc08c9b5634601
Abhijit Das <abhijitd721201@gmail.com>
2024-06-25 18:28:57 +0530

    file store using multer

--- Node Js/Multer_in_nodejs/app.js
+++ Node Js/Multer_in_nodejs/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
 
+
+const crypto = require('crypto')       
+const multer = require('multer')
+
 const app = express()
 
 app.use(express.json())
@@ -11,9 +15,36 @@ app.use(express.static(path.join(__dirname,"public")))
 
 app.set("view engine" , "ejs")
 
+// Disk Storage in multer..=> The disk storage engine gives you full control on storing files to disk.
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/upload')           // where we want to store the file
+    },
+    filename: function (req, file, cb) {    
+        crypto.randomBytes(12 , (err, bytes)=>{               // need a unic name to store the file
+            // console.log(bytes.toString("hex"))
+            const fn = bytes.toString("hex") + path.extname(file.originalname)
+
+            cb(null, fn)    // fn - file name
+        } )             
+         
+     
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
 
 app.get("/",(req,res)=>{
-    res.send("server is running")
+   
+    res.render("index")
+})
+
+
+// upload file 
+app.post("/upload", upload.single("image") ,(req,res)=>{
+    console.log(req.file)
+    res.redirect("/")
 })
 
 app.listen(3000 ,()=>{

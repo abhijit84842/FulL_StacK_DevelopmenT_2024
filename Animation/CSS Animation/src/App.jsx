5fe70d6f6bfc27ad211406267dad23992f7f6a72
Abhijit Das <abhijitd721201@gmail.com>
2024-11-01 21:06:57 +0530

    after and before animation apply

--- Animation/CSS Animation/src/App.jsx
+++ Animation/CSS Animation/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Animation Use in Website</h1>
-      <div className="box"></div>
+      <div className="box">Animation Box</div>
     </div>
   );
 }

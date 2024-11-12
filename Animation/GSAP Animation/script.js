0a3fc6a6f11c8bd5e701e495cfdf53a2bfad6ef3
Abhijit Das <abhijitd721201@gmail.com>
2024-11-12 12:11:35 +0530

    animation set in box

---
+++ Animation/GSAP Animation/script.js
@@ -0,0 +1,24 @@
+// from -> initial position
+// to -> final position
+
+// going to initial to final position
+// gsap.to("#box", {
+//   x: 1500,
+//   duration: 3,
+//   delay: 1,
+//   backgroundColor: "yellow", // write in camelCase
+//   rotate: 360,
+//   borderRadius: "50%",
+//   scale: 0.5,
+// });
+
+//  going to final to initial position
+gsap.from("#box",{
+    x: 1500,
+    duration: 3,
+    delay: 1,
+    backgroundColor: "yellow", // write in camelCase
+    rotate: 360,
+    borderRadius: "50%",
+    scale: 0.5,
+})

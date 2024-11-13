3cc8d72615dfa3ebe42f2ad9ddf6e7d58a085db3
Abhijit Das <abhijitd721201@gmail.com>
2024-11-13 16:51:34 +0530

    use lerp property

--- Animation/Locomotive Js/script.js
+++ Animation/Locomotive Js/script.js
@@ -2,5 +2,6 @@
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
-    smooth:true
+    smooth:true,
+    lerp:0.05   // Linear interpolation (lerp) intensity. Float between 0 and 1.
 })

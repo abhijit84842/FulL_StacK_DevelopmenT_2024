b6aaaf0f003b89027e51e175c6d974b553163a9f
Abhijit Das <abhijitd721201@gmail.com>
2024-11-25 12:06:13 +0530

    add loop and autoplay property

--- Animation/Swiper Js/script.js
+++ Animation/Swiper Js/script.js
@@ -2,5 +2,7 @@ var swiper = new Swiper('.swiper',{
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl:".swiper-button-prev",
-    }
+    },
+    loop:true,
+    autoplay:true,
 })

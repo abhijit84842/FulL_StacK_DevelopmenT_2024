66b2db5c253888e3802e513df82860306ff497fb
Abhijit Das <abhijitd721201@gmail.com>
2024-11-25 12:58:08 +0530

    apply framer motion on div

--- Animation/Framer Motion/src/App.jsx
+++ Animation/Framer Motion/src/App.jsx
@@ -3,7 +3,25 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <>
-      <motion.div id="box">Box 1</motion.div>
+      <motion.div
+        initial={{
+          x: 300,
+        }}
+        //final position
+        animate={{
+          x: 1000,
+          rotate: 360,
+        }}
+        transition={{
+          duration: 3,
+          delay: 1,
+          ease: "anticipate",
+          repeat: Infinity,
+        }}
+        id="box"
+      >
+        Box 1
+      </motion.div>
     </>
   );
 }

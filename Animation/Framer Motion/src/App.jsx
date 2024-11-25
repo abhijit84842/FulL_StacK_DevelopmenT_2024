eb067f8870fb3bd73aa3e014c5e2988ed7c7c19f
Abhijit Das <abhijitd721201@gmail.com>
2024-11-25 13:17:06 +0530

    apply 2 attribute whileDrag and whileHover

--- Animation/Framer Motion/src/App.jsx
+++ Animation/Framer Motion/src/App.jsx
@@ -14,20 +14,31 @@ function App() {
           button: 300,
         }}
         // Lock the drag direction
-        dragDirectionLock="true"
+        // dragDirectionLock="true"
+        whileHover={{
+          backgroundColor: "green",
+        }}
+        whileDrag={{
+          backgroundColor: "royalblue",
+          scale: 0.9,
+        }}
+        // whileTap={{
+        //   backgroundColor: "red",
+        //   scale: 0.5,
+        // }}
         initial={{
           x: 300,
         }}
         //final position
         animate={{
           x: 1000,
-          rotate: 360,
+          // rotate: 360,
         }}
         transition={{
           duration: 3,
           delay: 1,
           ease: "anticipate",
-          repeat: Infinity,
+          // repeat: Infinity,
         }}
         id="box"
       >
